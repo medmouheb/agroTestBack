021e62c54e5306ae0e009c212bbca4df38f458f1
khemiri <syrinekhemiri27@gmail.com>
2023-08-03 09:52:07 +0100

    ok

--- src/main/java/com/agrotech/api/services/impl/DeliveryInstructionServiceImpl.java
+++ src/main/java/com/agrotech/api/services/impl/DeliveryInstructionServiceImpl.java
@@ -7,11 +7,16 @@ import com.agrotech.api.exceptions.NotFoundException;
 import com.agrotech.api.mapper.CampanyMapper;
 import com.agrotech.api.mapper.DeliveryInstructionMapper;
 import com.agrotech.api.model.Campany;
+import com.agrotech.api.model.Currency;
 import com.agrotech.api.model.DeliveryInstruction;
+import com.agrotech.api.model.Division;
 import com.agrotech.api.services.DeliveryInstructionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -62,47 +67,87 @@ public class DeliveryInstructionServiceImpl implements DeliveryInstructionServic
 
     @Override
     public Page<DeliveryInstructionDto> findPage(int pageSize, int pageNumber, String filter) {
+
+
         return null;
     }
 
     @Override
-    public void delete(String s) throws NotFoundException {
-
+    public void delete(String id) throws NotFoundException {
+        if(!deliveryInstructionRepository.existsById(id)) {
+            throw new NotFoundException("Division not found ");
+        }
+        deliveryInstructionRepository.deleteById(id);
     }
 
     @Override
     public DeliveryInstructionDto findBytypeproduct(String producttype) throws NotFoundException {
-        return null;
+        Optional<DeliveryInstruction> divisionOptional = deliveryInstructionRepository.findByProductType(producttype);
+        if(divisionOptional.isEmpty()) {
+            throw new NotFoundException("Division not found ");
+        }
+        return deliveryInstructionMapper.toDto(divisionOptional.get());
     }
 
     @Override
-    public Page<DeliveryInstructionDto> findPage1(int pageSize, int pageNumber, String filter) {
-        return null;
+    public Page<DeliveryInstruction> findPage1(int pageSize, int pageNumber, String filter) {
+
+
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by("typeproduct").ascending());
+        Page<DeliveryInstruction>  result =  deliveryInstructionRepository.findByIsDeletedAndProductTypeContainingIgnoreCase(false,filter, pageable);
+        return result;
     }
 
     @Override
     public Page<DeliveryInstruction> getpages(int pageSize, int pageNumber, String filter) {
-        return null;
+
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by("productType").ascending());
+        Page<DeliveryInstruction> result =  deliveryInstructionRepository.findByIsDeleted(false, pageable);
+
+        return result;
+
+
     }
 
     @Override
     public Page<DeliveryInstruction> getpagesarchive(int pageSize, int pageNumber, String filter) {
-        return null;
+
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by("typeproduct").ascending());
+        Page<DeliveryInstruction> result =  deliveryInstructionRepository.findByIsDeleted(true, pageable);
+
+        return result;
+
+
+
     }
 
     @Override
     public void archive(String id) throws NotFoundException {
-
+        Optional<DeliveryInstruction> groOptional =  deliveryInstructionRepository.findById(id);
+        if(groOptional.isEmpty()) {
+            throw new NotFoundException("Currency not found ");
+        }
+        DeliveryInstruction groExisting = groOptional.get();
+        groExisting.setIsDeleted(true);
+        deliveryInstructionRepository.save(groExisting);
     }
 
     @Override
     public void setNotArchive(String id) throws NotFoundException {
-
+        Optional<DeliveryInstruction> groOptional =  deliveryInstructionRepository.findById(id);
+        if(groOptional.isEmpty()) {
+            throw new NotFoundException("Currency not found ");
+        }
+        DeliveryInstruction groExisting = groOptional.get();
+        groExisting.setIsDeleted(false);
+        deliveryInstructionRepository.save(groExisting);
     }
 
     @Override
-    public Page<DeliveryInstructionDto> findArchivedPage1(int pageSize, int pageNumber, String filter) {
-        return null;
+    public Page<DeliveryInstruction> findArchivedPage1(int pageSize, int pageNumber, String filter) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by("productType").ascending());
+        Page<DeliveryInstruction>  result =  deliveryInstructionRepository.findByIsDeletedAndProductTypeContainingIgnoreCase(true,filter, pageable);
+        return result;
     }
 
     @Override
